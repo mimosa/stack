8a0c28e28fe4ece8a89d43bc1639523507d78780
Howl王 <mimosa@shareday.com>
2013-06-18 18:04:04 +0800

    first commit

--- models/mongoid/base_model.rb
+++ models/mongoid/base_model.rb
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 require 'csv'
+require 'roo'
 
 module Mongoid
   module BaseModel
@@ -31,6 +32,20 @@ module Mongoid
         Hash[ field_list.collect{|column| [human_attribute_name(column), column]} ]
       end
 
+      def self.import(file)
+        spreadsheet = open_spreadsheet(file)
+
+        header = spreadsheet.row(1)
+        header = column_labels.slice(*header).values
+
+        (2..spreadsheet.last_row).each do |i|
+          row = Hash[[header, spreadsheet.row(i)].transpose]
+          product = find_by_id(row['id']) || new
+          product.attributes = row.to_hash.slice(*accessible_attributes)
+          product.save!
+        end
+      end
+
       def to_csv(field_list = nil)
         field_list ||= column_labels
         return false unless field_list.is_a?(Hash)
@@ -39,6 +54,21 @@ module Mongoid
           each do |record|
             csv << record.attributes.values_at(*field_list.values)
           end
+        end
+      end
+
+      private
+
+      def open_spreadsheet(file)
+        case File.extname(file.original_filename)
+          when ".csv" then 
+            ::Csv.new(file.path, nil, :ignore)
+          when ".xls" then 
+            ::Excel.new(file.path, nil, :ignore)
+          when ".xlsx" then 
+            ::Excelx.new(file.path, nil, :ignore)
+          else
+            raise "Unknown file type: #{file.original_filename}"
         end
       end
 
